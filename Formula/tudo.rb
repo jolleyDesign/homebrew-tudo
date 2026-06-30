a4bf28344b3cff08117a5df0a5776ff2b34b8c29
Alex Jolley <itisjolley+github@gmail.com>
2026-06-30 10:53:55 -0600

    tudo 1.0.4

--- Formula/tudo.rb
+++ Formula/tudo.rb
@@ -1,36 +1,48 @@
 class Tudo < Formula
-  desc "Fast, local-first todo list for your terminal"
-  homepage "https://github.com/jolleyDesign/tudo"
-  version "1.0.4"
-  license "MIT"
+    desc "Fast, local-first todo list for your terminal"
+    homepage "https://github.com/jolleyDesign/tudo"
+    version "1.0.4"
+    license "MIT"
 
-  on_macos do
-    on_arm do
-      url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.4/tudo-aarch64-apple-darwin.tar.gz"
-      sha256 "73eac6930d9df03a2d16698afc8126f50a5c419897f908b105d3f29db4fd09a4"
+    on_macos do
+      on_arm do
+        url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.4/tudo-aarch64-
+  apple-darwin.tar.gz"
+        sha256 "8ef09d266659046b02f481e167005327fa0bbd773f6458f8fdbd46d89f9c9051"
+      end
+      on_intel do
+        url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.4/tudo-x86_64-a
+  pple-darwin.tar.gz"
+        sha256 "aa2e891f23a21203d7a6ca28337d8491f6161c229f615547575fd2fde445424c"
+      end
     end
-    on_intel do
-      url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.4/tudo-x86_64-apple-darwin.tar.gz"
-      sha256 "e435a0df4021d185853fe7a4037b177987cd37e0f359d9493a003d50ef280c5e"
+
+    on_linux do
+      on_arm do
+        url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.4/tudo-aarch64-
+  unknown-linux-musl.tar.gz"
+        sha256 "75464258c11df751793eb5a599acedea68af2cc90930a4fefc259e85a4ad7dc5"
+      end
+    on_linux do
+      on_arm do                                                                         -apple-darwin.tar.gz"                                                                       end
+
+    on_linux do
+      on_arm do
+        url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.4/tudo-aarch64-
+  unknown-linux-musl.tar.gz"                                                                    sha256 "75464258c11df751793eb5a599acedea68af2cc90930a4fefc259e85a4ad7dc5"
+      end
+      on_intel do
+        url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.4/tudo-x86_64-u
+  nknown-linux-musl.tar.gz"
+        sha256 "ef7d6fa71dbce0322c4a756da6a899655c5b890f244ff3db07a960d138267140"
+      end
+    end
+
+    def install
+      bin.install "tudo"
+    end
+
+    test do
+      assert_match version.to_s, shell_output("#{bin}/tudo --version")
     end
   end
-
-  on_linux do
-    on_arm do
-      url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.4/tudo-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "b870c39689c6696ce8a70c7660a8e200ec01c336230f90d474732ea127215a64"
-    end
-    on_intel do
-      url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.4/tudo-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "3efd594dedca0e50ebc085a0d457a8b240df8b11588dac80fee2071565702daf"
-    end
-  end
-
-  def install
-    bin.install "tudo"
-  end
-
-  test do
-    assert_match version.to_s, shell_output("#{bin}/tudo --version")
-  end
-end
