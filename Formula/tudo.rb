a3a7e69a875c45aa449d94e59694e329c0a26a6d
Alex Jolley <itisjolley+github@gmail.com>
2026-07-06 18:12:34 -0600

    tudo v1.1.0

--- Formula/tudo.rb
+++ Formula/tudo.rb
@@ -1,28 +1,28 @@
 class Tudo < Formula
   desc "Fast, local-first todo list for your terminal"
   homepage "https://github.com/jolleyDesign/tudo"
-  version "1.0.6"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.6/tudo-aarch64-apple-darwin.tar.gz"
-      sha256 "bd810c750b01b2894351ab621dd235d0b1b4f2d799982064077c7f4716138bf5"
+      url "https://github.com/jolleyDesign/tudo/releases/download/v1.1.0/tudo-aarch64-apple-darwin.tar.gz"
+      sha256 "a326d90b42c4c3b13f6137e5f768caf561c5b6a0be8e41f2ed9a1e917380f33c"
     end
     on_intel do
-      url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.6/tudo-x86_64-apple-darwin.tar.gz"
-      sha256 "2a169199c15f3831f5d1b91efa40c69163deec59345b1d9ff003923d0b7a6101"
+      url "https://github.com/jolleyDesign/tudo/releases/download/v1.1.0/tudo-x86_64-apple-darwin.tar.gz"
+      sha256 "1e038467426051e4ca5fe36a6d52e7da9ca213dc3a766eae946e2bcff03c6850"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.6/tudo-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "03e9a621c536a44c89ccd106b2a45a541e76afda3a43ac40c398c4a9a3cd9161"
+      url "https://github.com/jolleyDesign/tudo/releases/download/v1.1.0/tudo-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "c9328ea77175008b734b975c3f882dcf40ffbd5061d3ceee983b6d80a7af0dab"
     end
     on_intel do
-      url "https://github.com/jolleyDesign/tudo/releases/download/v1.0.6/tudo-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "7df8b7700ece638e4b4beb4c37231efd6af6a76e76ee9ddbb0bbba4cfbcdb661"
+      url "https://github.com/jolleyDesign/tudo/releases/download/v1.1.0/tudo-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "3ec5b6a8800c1ed31bab1dcc54b7cff4e810422eea1fb6d56350c05d6763867d"
     end
   end
 
